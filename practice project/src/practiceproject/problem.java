b555613396d65214b96cf6f1f870ab0718c1d766
Pavitra Pidikiti <Pavitra Pidikiti@DESKTOP-KUUFCN8>
2021-08-03 20:22:44 +0530

    commit and push

--- practice project/src/practiceproject/problem.java
+++ practice project/src/practiceproject/problem.java
@@ -4,16 +4,8 @@ public class problem {
 
 	public static void main(String[] args) {
 		
-	}
-	 public int[] twoSum(int[] nums, int target) {
-	        for (int i = 0; i < nums.length; i++) {
-	            for (int j = i + 1; j < nums.length; j++) {
-	                if (nums[j] == target - nums[i]) {
-	                    return new int[] { i, j };
-	                }
-	            }
-	        }
-	        // In case there is no solution, we'll just return null
-	        return null;
-	    }
+	int n1 = 345, n2 = 175, sum;  
+	sum = n1 + n2;  
+	System.out.println("The sum of numbers is: "+sum);  
+}
 }
